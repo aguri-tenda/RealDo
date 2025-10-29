1e286a53e87f410b44156cd2d3eceafc37ec0e73
LauWaiLok <2401378@s.asojuku.ac.jp>
2025-10-29 12:02:01 +0900

    user+provider bottomの作成

---
+++ parts/user_bottom.php
@@ -0,0 +1,10 @@
+</body>
+<footer class="	#BDE3FF">
+    <div class="navbar-end">
+        <div class="navbar-item">
+            <div class="image is-3200x1800">
+                <img src="ロゴ.png" alt="RealDo">
+            </div>
+        </div>
+    </div>
+</footer>
