3a7c76c6fe06ebf6a4a9e6dbbb4daa552772e604
2401368 <2401368@s.asojuku.ac.jp>
2025-10-31 09:57:02 +0900

    Create index.php(利用者ホーム)
    利用者のホーム画面(index.php)を追加しました

---
+++ parts/index.php
@@ -0,0 +1,93 @@
+<div class="section" id="recommend-app">
+    <div class="container">
+        <div class="card" style="background-color: #FFFDDF;">
+            <div class="card-header">
+                <div class="card-header-icon">
+                    <i class="fas fa-thumbs-up"></i>
+                </div>
+                <div class="card-header-title">
+                    <span>あなたへのおすすめ</span>
+                </div>
+            </div>
+
+            <div class="card-content">
+                <div class="columns">
+
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-image">
+                                <div class="image is-256x256">
+                                    <img src="いちご狩り.png" alt="サムネイル">
+                                </div>
+                            </div>
+
+                            <div class="card-content">
+                                <div class="content">
+                                    <p><strong>いちご狩り</strong><button href="このタグ名で検索した検索結果画面URL(仮)" class="button is-small is-light is-rounded">タグ名</button></p>
+
+                                    <p>説明文～～～～～～～～～～</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-image">
+                                <div class="image is-256x256">
+                                    <img src="いちご狩り.png" alt="サムネイル">
+                                </div>
+                            </div>
+
+                            <div class="card-content">
+                                <div class="content">
+                                    <p><strong>いちご狩り</strong><button href="このタグ名で検索した検索結果画面URL(仮)" class="button is-small is-light is-rounded">タグ名</button></p>
+
+                                    <p>説明文～～～～～～～～～～</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-image">
+                                <div class="image is-256x256">
+                                    <img src="いちご狩り.png" alt="サムネイル">
+                                </div>
+                            </div>
+
+                            <div class="card-content">
+                                <div class="content">
+                                    <p><strong>いちご狩り</strong><button href="このタグ名で検索した検索結果画面URL(仮)" class="button is-small is-light is-rounded">タグ名</button></p>
+
+                                    <p>説明文～～～～～～～～～～</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-image">
+                                <div class="image is-256x256">
+                                    <img src="いちご狩り.png" alt="サムネイル">
+                                </div>
+                            </div>
+
+                            <div class="card-content">
+                                <div class="content">
+                                    <p><strong>いちご狩り</strong><button href="このタグ名で検索した検索結果画面URL(仮)" class="button is-small is-light is-rounded">タグ名</button></p>
+
+                                    <p>説明文～～～～～～～～～～</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
