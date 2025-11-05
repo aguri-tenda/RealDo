71bddeca67e31df9a6fb6834142517ae36195ed2
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-05 12:30:09 +0900

    a

--- parts/header.php
+++ parts/header.php
@@ -1,3 +1,25 @@
+<?php
+session_start();
+
+// 入力データを受け取り、セッションに保存
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_SESSION['user'] = [
+        'username' => $_POST['username'] ?? '',
+        'userid' => $_POST['userid'] ?? '',
+        'useraddress' => $_POST['useraddress'] ?? '',
+        'userpassword' => $_POST['userpassword'] ?? '',
+    ];
+}
+
+// セッションから値を取得
+$name = $_SESSION['user']['username'] ?? '';
+$id = $_SESSION['user']['userid'] ?? '';
+$address = $_SESSION['user']['useraddress'] ?? '';
+$password = $_SESSION['user']['userpassword'] ?? '';
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 
