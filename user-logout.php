0d7391523647f4a3da22b82b1e8b09bbe28dbf9d
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-06 11:59:24 +0900

    create logout

---
+++ user-logout.php
@@ -0,0 +1,19 @@
+<?php require "parts/header.php"; ?>
+<?php require "parts/navigation.php"; ?>
+
+<div class="level-item">
+    <form class="box" style="width: 520px; text-align: center;" action="user-logout-action.php" method="post">
+        <span class="subtitle is-4" style="color:#278EDD;">ログアウト確認</span>
+        <br><br><br>
+
+        <p style="color:#278EDD; font-size: 18px;">本当にログアウトしますか？</p>
+
+        <div class="field has-text-centered" style="margin-top: 2rem;">
+            <a href="index.php" class="button is-light is-medium" style="margin-right: 20px;">戻る</a>
+            <input class="button is-link is-medium" type="submit" value="ログアウト"
+                style="background-color: #41C0FF; width: 40%;">
+        </div>
+    </form>
+</div>
+<?php require "parts/user_bottom.php"; ?>
+<?php require "parts/footer.php"; ?>
