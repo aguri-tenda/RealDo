76324a39df8b83df5faac2301e238aa1f45eec66
LauWaiLok <2401378@s.asojuku.ac.jp>
2025-11-06 11:23:48 +0900

    lau

--- parts/provider_bottom.php
+++ parts/provider_bottom.php
@@ -1,4 +1,4 @@
-<footer class="footer" style="background-color:#C3FF9B; position: fixed; bottom: 0; width: 100%; padding: 0;">
+<footer class="footer" style="background-color:#C3FF9B; position: fixed; bottom: 0; width: 100%; padding: 3px;">
     <div class="navbar-end">
         <div class="image" style="width: 100px; height: auto;">
             <img src="img/logo.png" alt="RealDo">
