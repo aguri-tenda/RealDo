405999fc2db53f35f974092c1b3b47a03c6cb507
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-06 11:41:10 +0900

    a

---
+++ detail.php
@@ -0,0 +1,9 @@
+<?php require "parts/header.php"; ?>
+<?php require "parts/navigation.php"; ?>
+<?php require "parts/db-connect.php"; ?>
+
+<div class="container" style="margin-top: 2rem; margin-bottom: 2rem;">
+</div>
+
+<?php require "parts/provider_bottom.php"; ?>
+<?php require "parts/footer.php"; ?>
