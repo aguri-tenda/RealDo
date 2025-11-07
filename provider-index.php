cade68a8fff41908222385c95910a1a338252478
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-07 12:04:09 +0900

    Update provider-index.php

--- provider-index.php
+++ provider-index.php
@@ -26,7 +26,30 @@ $products = $sql->fetchAll(PDO::FETCH_ASSOC);
         </aside>
     </div>
 
-    <div class="column">
+<div class="column">
+        <h1 class="title is-3" style="margin: 25px;">商品一覧</h1>
+        <hr style="margin-top: -10px;">
+
+        <?php if (!empty($products)): ?>
+            <?php foreach ($products as $product): ?>
+                <div class="box" style="margin: 25px; display: flex; align-items: center;">
+                    <div style="flex-grow: 1;">
+                        <p class="title is-4"><?= htmlspecialchars($product['name']) ?></p>
+                        <span class="tag is-success is-light" style="margin-bottom: 10px;">体験</span>
+                        <p><strong>所在地:</strong> <?= htmlspecialchars($product['address']) ?></p>
+                        <p><strong>参加人数:</strong> <?= htmlspecialchars($product['max_participants']) ?>/<?= htmlspecialchars($product['max_participants']) ?>人</p>
+                    </div>
+                    <div style="flex-shrink: 0; margin-left: 20px;">
+                        <img src="img/<?= htmlspecialchars($product['image_path'] ?? 'default_product.png') ?>" alt="<?= htmlspecialchars($product['product_name']) ?>" style="width: 150px; height: 100px; object-fit: cover; border-radius: 5px;">
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <div class="box" style="margin: 25px;">
+                <p>現在、登録されている商品はありません。</p>
+                <a href="product-insert.php" class="button is-link is-light" style="margin-top: 15px;">商品登録はこちらから</a>
+            </div>
+        <?php endif; ?>
 
     </div>
 </div>
