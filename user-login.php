00dc63920f94f5281c5349b5ae277f7872490379
LauWaiLok <2401378@s.asojuku.ac.jp>
2025-11-07 10:42:23 +0900

    Lau

--- user-login.php
+++ user-login.php
@@ -1,31 +1,37 @@
 <?php require "parts/header.php"; ?>
 <?php require "parts/navigation.php"; ?>
-
+<br>
 <div class="level-item">
-    <form class="box" style="width: 800px; text-align: center;">
-        <span class="subtitle is-4" style="color:#278EDD;">ログイン</span>
-        <br>
+    <form action="user-output-action.php" method="post" class="box"
+        style="max-width: 550px; width: 100%; height:450px; text-align: center; margin: auto; padding: 2rem;">
+        <h2 class="subtitle is-4" style="color: #278EDD;">ログイン</h2>
+        <br><br><br>
+
+        <!-- ユーザーID -->
         <div class="field is-horizontal">
             <div class="field-label is-normal">
-                <label style="color:#278EDD;">ユーザーID</label>
+                <label style="color: #278EDD;">ユーザーID</label>
             </div>
             <div class="field-body">
                 <div class="field">
                     <div class="control">
-                        <input class="input" type="text" name="userid" style="background-color: #D9D9D9;">
+                        <input class="input" type="text" name="userid" required placeholder="ユーザーIDを入力"
+                            style="background-color: #D9D9D9; border: 1px solid #858484; width: 90%;">
                     </div>
                 </div>
             </div>
         </div>
 
+        <!-- パスワード -->
         <div class="field is-horizontal">
             <div class="field-label is-normal">
-                <label style="color:#278EDD;">パスワード</label>
+                <label style="color: #278EDD;">パスワード</label>
             </div>
             <div class="field-body">
                 <div class="field">
                     <div class="control">
-                        <input class="input" type="password" name="password" style="background-color: #D9D9D9;">
+                        <input class="input" type="password" name="password" required placeholder="パスワードを入力"
+                            style="background-color: #D9D9D9; border: 1px solid #858484; width: 90%;">
                     </div>
                 </div>
             </div>
@@ -33,9 +39,10 @@
 
         <!-- ボタン -->
         <div class="field has-text-centered" style="margin-top: 2rem;">
-            <input class="button is-info" type="submit" value="確定" style="background-color: #41C0FF; width: 300px;">
+            <button class="button is-info" style="background-color: #41C0FF; width: 225px; border-radius: 5px;">
+                確定
+            </button>
         </div>
-
     </form>
 </div>
 
