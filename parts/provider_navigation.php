52170c979bef21b77ac22a7e0ec6aebd30fdfc53
LauWaiLok <2401378@s.asojuku.ac.jp>
2025-11-07 11:50:31 +0900

    lau

--- parts/provider_navigation.php
+++ parts/provider_navigation.php
@@ -1,7 +1,7 @@
 <div id="app-navigation">
     <nav class="navbar is-light" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-            <a class="navbar-item" href="./index.php">
+            <a class="navbar-item" href="./provider-index.php">
                 <img src="img/logo.png" alt="RealDo" style="max-height: 60px;">
             </a>
 
@@ -21,12 +21,12 @@
                 <!-- ユーザー登録 or 更新 -->
                 <div class="navbar-item">
                     <?php if (isset($_SESSION['provider'])): ?>
-                        <a href="./userUpdate.php" class="is-flex is-align-items-center">
+                        <a href="./providerUpdate.php" class="is-flex is-align-items-center">
                             <span class="icon has-text-info"><i class="far fa-edit"></i></span>
                             <span>ユーザー情報更新</span>
                         </a>
                     <?php else: ?>
-                        <a href="./userInsert.php" class="is-flex is-align-items-center">
+                        <a href="./providerInsert.php" class="is-flex is-align-items-center">
                             <span class="icon has-text-info"><i class="far fa-edit"></i></span>
                             <span>ユーザー新規登録</span>
                         </a>
@@ -36,12 +36,12 @@
                 <!-- ログイン or ログアウト -->
                 <div class="navbar-item">
                     <?php if (isset($_SESSION['provider'])): ?>
-                        <a href="./user-logout.php" class="is-flex is-align-items-center">
+                        <a href="./provider-logout.php" class="is-flex is-align-items-center">
                             <span class="icon has-text-info"><i class="fas fa-sign-out-alt"></i></span>
                             <span>ログアウト</span>
                         </a>
                     <?php else: ?>
-                        <a href="./user-login.php" class="is-flex is-align-items-center">
+                        <a href="./provider-login.php" class="is-flex is-align-items-center">
                             <span class="icon has-text-info"><i class="fas fa-sign-in-alt"></i></span>
                             <span>ログイン</span>
                         </a>
