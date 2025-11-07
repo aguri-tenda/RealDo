5145a93e8d3195867f6892a0cc28c46b09891cd0
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-07 11:58:25 +0900

    Update provider-index.php

--- provider-index.php
+++ provider-index.php
@@ -2,6 +2,13 @@
 <?php require "parts/provider_navigation.php"; ?>
 <?php require "parts/db-connect.php"; ?>
 
+<?php
+// 商品一覧を取得
+$sql = $pdo->prepare("SELECT * FROM products WHERE provider_id = ?");
+$sql->execute([$provider_id]);
+$products = $sql->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <div class="columns is-gapless">
     <div class="column is-narrow" style="background-color: #79D159; min-height: 100vh; padding: 0;">
         <aside class="menu" style="padding: 25px; width: 250px;">
