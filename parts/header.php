6eb0a672691ede9a26e414d09243ff79d8e2a911
LauWaiLok <2401378@s.asojuku.ac.jp>
2025-11-07 12:15:46 +0900

    lau

--- parts/header.php
+++ parts/header.php
@@ -6,6 +6,11 @@ $name = $_SESSION['user']['username'] ?? '';
 $user_id = $_SESSION['user']['userid'] ?? '';
 $address = $_SESSION['user']['useraddress'] ?? '';
 $password = $_SESSION['user']['userpassword'] ?? '';
+
+$provider_name = $_SESSION['provider']['providername'] ?? '';
+$provider_id = $_SESSION['provider']['providerid'] ?? '';
+$provider_address = $_SESSION['provider']['provideraddress'] ?? '';
+$provider_password = $_SESSION['provider']['providerpassword'] ?? '';
 ?>
 
 
