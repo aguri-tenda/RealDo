f625dcdd363ebbdee2705a7107a8284076808e07
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-07 11:39:51 +0900

    Update provider-index.php

--- provider-index.php
+++ provider-index.php
@@ -4,16 +4,16 @@
 
 <div class="columns is-gapless">
     <div class="column is-narrow" style="background-color: #79D159; min-height: 100vh; padding: 0;">
-        <aside class="menu" style="padding: 20px; width: 250px;">
+        <aside class="menu" style="padding: 25px; width: 250px;">
             <ul class="menu-list">
                 <li>
                     <a href="#" class="is-active" style="background-color: #55B537; color: white;">商品一覧</a>
                 </li>
                 <li>
-                    <a href="productInsert.php" style="color: white;">商品登録</a>
+                    <a href="product-insert.php" style="color: white;">商品登録</a>
                 </li>
                 <li>
-                    <a href="productManagement.php" style="color: white;">商品管理</a>
+                    <a href="product-update.php" style="color: white;">商品管理</a>
                 </li>
             </ul>
         </aside>
