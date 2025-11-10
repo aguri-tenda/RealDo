ce018bd6ad01a28edefddb29a9154b8085ec013e
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-10 11:35:57 +0900

    tochu

--- provider-login.php
+++ provider-login.php
@@ -1,31 +1,36 @@
 <?php require "parts/header.php"; ?>
 <?php require "parts/provider_navigation.php"; ?>
-
+<br>
 <div class="level-item">
-    <form class="box" style="width: 800px; text-align: center;">
-        <span class="subtitle is-4" style="color:#278EDD;">ログイン</span>
-        <br>
+    <form class="box" style="max-width: 700px; width: 100%; text-align: center;" action="provider-login-output.php" method="post">
+        <h2 class="subtitle is-4" style="color: #27ea6bff;">ログイン</h2>
+        <br><br><br>
+
+        <!-- ユーザーID -->
         <div class="field is-horizontal">
             <div class="field-label is-normal">
-                <label style="color:#278EDD;">ユーザーID</label>
+                <label style="color: #27ea6bff;">ユーザーID</label>
             </div>
             <div class="field-body">
                 <div class="field">
                     <div class="control">
-                        <input class="input" type="text" name="userid" style="background-color: #D9D9D9;">
+                        <input class="input" type="text" name="userid" required placeholder="ユーザーIDを入力"
+                            style="background-color: #D9D9D9; border: 1px solid #858484; width: 90%;">
                     </div>
                 </div>
             </div>
         </div>
 
+        <!-- パスワード -->
         <div class="field is-horizontal">
             <div class="field-label is-normal">
-                <label style="color:#278EDD;">パスワード</label>
+                <label style="color: #27ea6bff;">パスワード</label>
             </div>
             <div class="field-body">
                 <div class="field">
                     <div class="control">
-                        <input class="input" type="password" name="password" style="background-color: #D9D9D9;">
+                        <input class="input" type="password" name="password" required placeholder="パスワードを入力"
+                            style="background-color: #D9D9D9; border: 1px solid #858484; width: 90%;">
                     </div>
                 </div>
             </div>
@@ -33,15 +38,16 @@
 
         <!-- ボタン -->
         <div class="field has-text-centered" style="margin-top: 2rem;">
-            <input class="button is-info" type="submit" value="確定" style="background-color: #C3FF9B; width: 300px;">
+            <button class="button is-info" style="background-color: #27ea6bff; width: 225px; border-radius: 5px;">
+                確定
+            </button>
         </div>
-
     </form>
 </div>
 <br>
 <div class="field has-text-centered">
-    <a href="user-login.php" class="button is-info is-medium"
-        style="background-color: #23c8ffff; width: 250px;">ユーザーログイン</a>
+    <a href="provider-login.php" class="button is-info is-medium"
+        style="background-color: #41C0FF; width: 250px;">ユーザーログイン</a>
 </div>
 
 <?php require "parts/provider_bottom.php"; ?>
