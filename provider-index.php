0270c92c62b5434bee733e4300b89d48216ac3aa
LauWaiLok <2401378@s.asojuku.ac.jp>
2025-11-10 11:48:44 +0900

    lau

--- provider-index.php
+++ provider-index.php
@@ -11,22 +11,25 @@ $products = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 <div class="columns is-gapless">
     <div class="column is-narrow" style="background-color: #79D159; min-height: 100vh; padding: 0;">
-        <aside class="menu" style="padding: 25px; width: 250px;">
+        <aside class="menu" style="padding: 25px; width: 200px;">
             <ul class="menu-list">
                 <li>
                     <a href="#" class="is-active" style="background-color: #55B537; color: white;">商品一覧</a>
                 </li>
+                <hr>
                 <li>
                     <a href="product-insert.php" style="color: white;">商品登録</a>
                 </li>
+                <hr>
                 <li>
                     <a href="product-update.php" style="color: white;">商品管理</a>
                 </li>
+                <hr>
             </ul>
         </aside>
     </div>
 
-<div class="column">
+    <div class="column">
         <h1 class="title is-3" style="margin: 25px;">商品一覧</h1>
         <hr style="margin-top: -10px;">
 
@@ -37,10 +40,14 @@ $products = $sql->fetchAll(PDO::FETCH_ASSOC);
                         <p class="title is-4"><?= htmlspecialchars($product['name']) ?></p>
                         <span class="tag is-success is-light" style="margin-bottom: 10px;">体験</span>
                         <p><strong>所在地:</strong> <?= htmlspecialchars($product['address']) ?></p>
-                        <p><strong>参加人数:</strong> <?= htmlspecialchars($product['max_participants']) ?>/<?= htmlspecialchars($product['max_participants']) ?>人</p>
+                        <p><strong>参加人数:</strong>
+                            <?= htmlspecialchars($product['max_participants']) ?>/<?= htmlspecialchars($product['max_participants']) ?>人
+                        </p>
                     </div>
                     <div style="flex-shrink: 0; margin-left: 20px;">
-                        <img src="img/<?= htmlspecialchars($product['image_path'] ?? 'default_product.png') ?>" alt="<?= htmlspecialchars($product['product_name']) ?>" style="width: 150px; height: 100px; object-fit: cover; border-radius: 5px;">
+                        <img src="img/<?= htmlspecialchars($product['image_path'] ?? 'default_product.png') ?>"
+                            alt="<?= htmlspecialchars($product['product_name']) ?>"
+                            style="width: 150px; height: 100px; object-fit: cover; border-radius: 5px;">
                     </div>
                 </div>
             <?php endforeach; ?>
@@ -55,6 +62,9 @@ $products = $sql->fetchAll(PDO::FETCH_ASSOC);
 </div>
 
 
+
+<?php require "parts/provider_bottom.php"; ?>
+<?php require "parts/footer.php"; ?>
 
 <?php require "parts/provider_bottom.php"; ?>
 <?php require "parts/footer.php"; ?>
