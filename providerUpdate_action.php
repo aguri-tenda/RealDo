963511cf0d01dcb8ecb5b3e494dfa37205482bcc
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-11 11:32:01 +0900

    a

--- providerUpdate_action.php
+++ providerUpdate_action.php
@@ -3,44 +3,46 @@
 <?php
 
 // セッションからデータを取得
-$providername = $_SESSION['provider_input']['providername'] ?? '';
-$providerid = $_SESSION['provider_input']['providerid'] ?? '';
-$provideraddress = $_SESSION['provider_input']['provideraddress'] ?? '';
-$providerpassword_raw = $_SESSION['provider_input']['providerpassword'] ?? ''; // 平文のパスワード
+$name = $_SESSION['provider_update']['name'] ?? '';
+$providerid = $_SESSION['provider_update']['provider_id'] ?? '';
+$provideraddress = $_SESSION['provider_update']['address'] ?? '';
+$providerpassword_raw = $_SESSION['provider_update']['password'] ?? ''; // 平文のパスワード
 
 if (!empty($providerid) && !empty($providerpassword_raw)) {
     $hashed_password = password_hash($providerpassword_raw, PASSWORD_DEFAULT);
 
     // DBに登録
-    $sql = $pdo->prepare("INSERT INTO providers (provider_id, name, mail, password) VALUES (?, ?, ?, ?)");
+    $sql = $pdo->prepare("UPDATE providers SET provider_id = ?, name = ?, mail = ?, password = ? WHERE provider_id = ?");
     
     $success = $sql->execute([
         $providerid,
         $providername,
         $provideraddress,
         $hashed_password,
+        $_SESSION['provider']['provider_id']
     ]);
 
     if ($success) {
         // 登録が成功した場合、セッション（登録用のユーザー情報）をクリアする
-        unset($_SESSION['provider_input']);
+        unset($_SESSION['provider_update']);
         $_SESSION['provider'] = [
             'providername' => $providername,
             'provideraddress' => $provideraddress,
-            'providerid' => $providerid
+            'providerid' => $providerid,
         ];
 
         // index.phpへリダイレクト
-        header('Location: provider-index.php');
+        header('Location: index.php');
         exit;
     } else {
         // 登録失敗時のエラー処理（例: ログ出力やエラー画面への遷移）
+        header('Location: error.php');
+        exit;
     }
 
 } else {
-    // 必須データがない場合はエラー画面などへリダイレクト
     // index.phpへリダイレクト
-    header('Location: provider-index.php');
+    header('Location: index.php');
     exit;
 }
 
