b1b25f116e5c705a43fd3ba578153135ddca14df
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-14 09:54:41 +0900

    Update product-management.php

--- product-management.php
+++ product-management.php
@@ -35,66 +35,66 @@
     </div>
 
     <div class="column">
-        <?php if( isset($_SESSION['provider'])) : ?>
-        <?php
-        $sql = $pdo->prepare("SELECT * FROM products WHERE provider_id = ?");
-        $sql->execute([$_SESSION['provider']['providerid']]);
-        $products = $sql->fetchAll(PDO::FETCH_ASSOC);
-        ?>
-
-        <?php if ($products): ?>
-        <?php foreach ($products as $product): ?>
-            
+        <?php if( $islogin) : ?>
             <?php
-                $tag = $pdo->prepare( "SELECT * FROM attached_tags JOIN tags ON attached_tags.tag_id = tags.tag_id WHERE product_id = ? ;" );
-                $tag->execute([ $product['product_id'] ]);
+            $sql = $pdo->prepare("SELECT * FROM products WHERE provider_id = ?");
+            $sql->execute([$_SESSION['provider']['providerid']]);
+            $products = $sql->fetchAll(PDO::FETCH_ASSOC);
             ?>
 
-            <div class="box" style="margin: 25px; display: flex; align-items: center;">
-                <div style="flex-grow: 1;">
-                    <p>
-                        <span class="title is-4"><?= htmlspecialchars($product['name']) ?></span>
+            <?php if ($products): ?>
+                <?php foreach ($products as $product): ?>
+                    
+                    <?php
+                        $tag = $pdo->prepare( "SELECT * FROM attached_tags JOIN tags ON attached_tags.tag_id = tags.tag_id WHERE product_id = ? ;" );
+                        $tag->execute([ $product['product_id'] ]);
+                    ?>
 
-                        <?php foreach( $tag as $tags ) : ?>
-                            <span><button class="button is-small is-primary is-outlined is-rounded" disabled><?= $tags['name']; ?></button></span>
-                        <?php endforeach; ?>
-                    </p>
+                    <div class="box" style="margin: 25px; display: flex; align-items: center;">
+                        <div style="flex-grow: 1;">
+                            <p>
+                                <span class="title is-4"><?= htmlspecialchars($product['name']) ?></span>
 
-                    <p><strong>場所:</strong> <?= htmlspecialchars($product['location']) ?></p>
-                    <p><strong>所在地:</strong> <?= htmlspecialchars($product['address']) ?></p>
-                    <p><strong>参加人数:</strong>
-                        <?= htmlspecialchars($product['max_participants']) ?>/<?= htmlspecialchars($product['max_participants']) ?>人
-                    </p>
-                </div>
-                <div style="flex-shrink: 0; margin-left: 20px;">
-                    <img src="<?= htmlspecialchars($product['image_pass']) ?>"
-                        alt="<?= htmlspecialchars($product['name']) ?>"
-                        style="width: 150px; height: 100px; object-fit: cover; border-radius: 5px;">
-                </div>
-            </div>
+                                <?php foreach( $tag as $tags ) : ?>
+                                    <span><button class="button is-small is-primary is-outlined is-rounded" disabled><?= $tags['name']; ?></button></span>
+                                <?php endforeach; ?>
+                            </p>
 
-            <div class="media-right">
-                <form action="provider-reservation-info.php" method="post">
-                    <input type="hidden" name="product_id" value="<?= $product['product_id']; ?>">
+                            <p><strong>場所:</strong> <?= htmlspecialchars($product['location']) ?></p>
+                            <p><strong>所在地:</strong> <?= htmlspecialchars($product['address']) ?></p>
+                            <p><strong>参加人数:</strong>
+                                <?= htmlspecialchars($product['max_participants']) ?>/<?= htmlspecialchars($product['max_participants']) ?>人
+                            </p>
+                        </div>
+                        <div style="flex-shrink: 0; margin-left: 20px;">
+                            <img src="<?= htmlspecialchars($product['image_pass']) ?>"
+                                alt="<?= htmlspecialchars($product['name']) ?>"
+                                style="width: 150px; height: 100px; object-fit: cover; border-radius: 5px;">
+                        </div>
+                    </div>
 
-                    <button class="button is-primary is-rounded">予約情報を見る</button>
-                </form>
+                    <div class="media-right">
+                        <form action="provider-reservation-info.php" method="post">
+                            <input type="hidden" name="product_id" value="<?= $product['product_id']; ?>">
 
-                <script>
-                    function deleteAlert( name, id )
-                    {
-                        if( confirm( name + "の掲載を取りやめます。\nよろしいですか？" ))
-                        {
-                            window.location.href="product-delete-output.php?product_id=" + id;
-                        }
-                    }
-                </script>
-                <button class="button is-danger is-rounded" onclick="deleteAlert('<?= htmlspecialchars($product['name'], ENT_QUOTES) ; ?>', <?= $product['product_id'] ; ?>)">商品を削除</button>
-        
-            </div>
-        <?php endforeach; ?>
-            <?php else : ?>
-                <p>まだ商品が登録されていません。</p>
+                            <button class="button is-primary is-rounded">予約情報を見る</button>
+                        </form>
+
+                        <script>
+                            function deleteAlert( name, id )
+                            {
+                                if( confirm( name + "の掲載を取りやめます。\nよろしいですか？" ))
+                                {
+                                    window.location.href="product-delete-output.php?product_id=" + id;
+                                }
+                            }
+                        </script>
+                        <button class="button is-danger is-rounded" onclick="deleteAlert('<?= htmlspecialchars($product['name'], ENT_QUOTES) ; ?>', <?= $product['product_id'] ; ?>)">商品を削除</button>
+                
+                    </div>
+                <?php endforeach; ?>
+                <?php else : ?>
+                    <p>まだ商品が登録されていません。</p>
             <?php endif; ?>
 
         <?php else : ?>
