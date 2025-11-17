c20e9ab4917081832cb10ad7ae3a6b0dd8dab0e0
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-17 12:53:16 +0900

    Update navigation.php

--- parts/navigation.php
+++ parts/navigation.php
@@ -62,7 +62,7 @@ $tags = $sql->fetchAll(PDO::FETCH_ASSOC);
                     <?php endif; ?>
                 </div>
                 <div class="navbar-item">
-                    <?php if (isset($_SESSION['provider'])): ?>
+                    <?php if (!isset($_SESSION['provider'])): ?>
                         <a href="./provider-login.php" class="is-flex is-align-items-center">
                             <span class="icon has-text-info"><i class="fas fa-user-cog"></i></span>
                             <span>提供者</span>
