88f7b73e106b3869f581c220aa9f3ddfb9f90bb7
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-18 12:45:23 +0900

    Update detail.php

--- detail.php
+++ detail.php
@@ -27,10 +27,28 @@ $dates = $dates_sql->fetchAll(PDO::FETCH_ASSOC);
 
 
 <div class="container has-text-centered" style="margin-top: 2rem; margin-bottom: 2rem;">
-    <div class="field is-horizontal" style="padding: 2rem;">
-        <figure class="image is-4by3">
+    <div class="field is-horizontal is-justify-content-center mt-5">
+        <figure class="image is-256x256 is-inline-block">
             <img src="<?= htmlspecialchars($product['image_pass']) ?>" alt="<?= htmlspecialchars($product['name']) ?>">
         </figure>
+    </div>
+
+    <div class="box" style="margin: 25px; display: flex; align-items: center;">
+        <div style="flex-grow: 1;">
+            <p>
+                <span class="title is-4"><?= htmlspecialchars($product['name']) ?></span>
+
+                <?php foreach( $tag as $tags ) : ?>
+                    <span><button class="button is-small is-primary is-outlined is-rounded" disabled><?= $tags['name']; ?></button></span>
+                <?php endforeach; ?>
+            </p>
+
+            <p><strong>場所:</strong> <?= htmlspecialchars($product['location']) ?></p>
+            <p><strong>所在地:</strong> <?= htmlspecialchars($product['address']) ?></p>
+            <p><strong>参加人数:</strong>
+                <?= htmlspecialchars($product['max_participants']) ?>/<?= htmlspecialchars($product['max_participants']) ?>人
+            </p>
+        </div>
     </div>
 </div>
 
