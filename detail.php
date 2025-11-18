e97ed5253784cb86b62a2fc89cd65448615d9e20
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-18 12:17:36 +0900

    Update detail.php

--- detail.php
+++ detail.php
@@ -8,9 +8,19 @@ if ($product_id === null) {
     echo "<p>Product ID is missing.</p>";
     exit;
 }
-$sql = $pdo->prepare("SELECT * FROM products WHERE id = ?");
+$sql = $pdo->prepare("SELECT * FROM products WHERE product_id = ?");
 $sql->execute([$product_id]);
 $product = $sql->fetch(PDO::FETCH_ASSOC);
+$tags_sql = $pdo->prepare(
+    "SELECT t.tag_name
+    FROM attached_tags att
+    INNER JOIN tags t
+        ON att.tag_id = t.tag_id
+    WHERE att.product_id = ?"
+);
+$tags_sql->execute([$product_id]);
+$tags = $tags_sql->fetchAll(PDO::FETCH_ASSOC);
+$dates_sql = $pdo->prepare("SELECT * FROM dates WHERE product_id = ? ORDER BY date ASC");
 ?>
 
 
@@ -21,9 +31,6 @@ $product = $sql->fetch(PDO::FETCH_ASSOC);
                 <figure class="image is-4by3">
                     <img src="<?= htmlspecialchars($product['image_pass']) ?>" alt="Product Image">
                 </figure>
-            </div>
-            <div class="column">
-                
             </div>
         </div>
     </div>
