7ccf451d0cc6319b54bad6b890f27789b973ab18
aguri-tenda <2401388@s.asojuku.ac.jp>
2025-11-19 12:21:56 +0900

    Update product-update-output.php

--- product-update-output.php
+++ product-update-output.php
@@ -24,6 +24,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 
 <?php require "parts/db-connect.php"; ?>
+<?php require "parts/address.php"; ?>
 
 <div class="has-background-light">
     <?php
@@ -202,8 +203,10 @@
                                         $gettag->execute([ $_GET['product_id'] ]);
 
 
-                                        $sql = $pdo->prepare(" UPDATE products SET name = ?, location = ?, post_code = ?, address = ?, detail = ?, image_pass = ?, price = ?, tel = ?, max_participants = ? WHERE product_id = ? ");
-                                        $sql->execute([ $name, $location, $post_code, $address, $detail, $image_pass, $price, $tel, $max, $_GET['product_id'] ]);
+                                        $sql = $pdo->prepare(
+                                            "UPDATE products SET name = ?, location = ?, post_code = ?, address = ?, detail = ?, image_pass = ?, price = ?, tel = ?, max_participants = ?, area = ?
+                                            WHERE product_id = ? ");
+                                        $sql->execute([ $name, $location, $post_code, $address, $detail, $image_pass, $price, $tel, $max, $area, $_GET['product_id'] ]);
                                     ?>
 
                                 <div class="level">
