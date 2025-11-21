1e7e4394eab94f5988a3101e13588ad8f8c1191c
LauWaiLok <2401378@s.asojuku.ac.jp>
2025-11-21 12:34:31 +0900

    Lau

--- booking_complete.php
+++ booking_complete.php
@@ -109,7 +109,7 @@ $tels = $_POST['booking_tel'] ?? [];
 
         <!-- ボタン -->
         <div class="field has-text-centered" style="margin-top: 2rem;">
-            <a href="booking_complete.php" class="button is-light is-medium" style="margin-right: 20px;">
+            <a href="booking_confirm.php" class="button is-light is-medium" style="margin-right: 20px;">
                 戻る
             </a>
 
