809f1631bb8834529969f04186570b7710aa001c
LauWaiLok <2401378@s.asojuku.ac.jp>
2025-11-21 12:34:52 +0900

    Lau

--- parts/header.php
+++ parts/header.php
@@ -38,7 +38,7 @@ $provider_address = $_SESSION['provider']['provideraddress'] ?? '';
         .sidebar {
             background-color: #79D159;
             min-height: 100vh;
-            padding: 0px;
+            padding: 0;
         }
 
         .menu {
