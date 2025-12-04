d3ab78fa541f3a24624589f38f786dba76135cee
2401368 <2401368@s.asojuku.ac.jp>
2025-12-04 11:50:27 +0900

    purchase-active-flag修正
    is_activeを0にするSQLの重複の解消

--- purchase-active-flag.php
+++ purchase-active-flag.php
@@ -3,9 +3,6 @@
 <?php
     if( isset( $_GET['purchase_id'] ) )
     {
-        $sql = $pdo->prepare( "UPDATE purchases SET is_active = !is_active WHERE purchase_id = ? ;" );
-        $sql->execute([ $_GET['purchase_id'] ]);
-
         $getPurchases = $pdo->prepare(" SELECT * FROM purchases WHERE purchase_id = ? AND is_active = 1; ");
         $getPurchases->execute([ $_GET['purchase_id'] ]);
         $getPurchases->fetch();
@@ -14,7 +11,7 @@
         {
             foreach( $getPurchases as $purchases )
             {
-                $purchaseUnactive = $pdo->prepare(" UPDATE purchases SET is_active = 0 WHERE purchase_id = ?; ");
+                $purchaseUnactive = $pdo->prepare(" UPDATE purchases SET is_active = !is_active WHERE purchase_id = ?; ");
                 $purchaseUnactive->execute([ $_GET['purchase_id'] ]);
                 $count_sql = $pdo->prepare(" SELECT SUM( attendance ) AS total FROM purchases WHERE product_id = ? AND start_time = ? AND is_active = 1; ");
                 $count_sql->execute([ $purchases['product_id'], $purchases['start_time'] ]);
