49d01854b4682794891f50506b164e5e1ef9a24e
2401368 <2401368@s.asojuku.ac.jp>
2025-12-04 09:37:24 +0900

    予約取り消しSQL修正
    参加人数の取得にis_active=1を追加（取り消し後にis_activeを0にする処理も追記）

--- purchase-active-flag.php
+++ purchase-active-flag.php
@@ -6,7 +6,7 @@
         $sql = $pdo->prepare( "UPDATE purchases SET is_active = !is_active WHERE purchase_id = ? ;" );
         $sql->execute([ $_GET['purchase_id'] ]);
 
-        $getPurchases = $pdo->prepare(" SELECT * FROM purchases WHERE purchase_id = ?; ");
+        $getPurchases = $pdo->prepare(" SELECT * FROM purchases WHERE purchase_id = ? AND is_active = 1; ");
         $getPurchases->execute([ $_GET['purchase_id'] ]);
         $getPurchases->fetch();
 
@@ -14,7 +14,9 @@
         {
             foreach( $getPurchases as $purchases )
             {
-                $count_sql = $pdo->prepare(" SELECT SUM( attendance ) AS total FROM purchases WHERE product_id = ? AND start_time = ? ; ");
+                $purchaseUnactive = $pdo->prepare(" UPDATE purchases SET is_active = 0 WHERE purchase_id = ?; ");
+                $purchaseUnactive->execute([ $_GET['purchase_id'] ]);
+                $count_sql = $pdo->prepare(" SELECT SUM( attendance ) AS total FROM purchases WHERE product_id = ? AND start_time = ? AND is_active = 1; ");
                 $count_sql->execute([ $purchases['product_id'], $purchases['start_time'] ]);
 
                 foreach( $count_sql as $attendance )
